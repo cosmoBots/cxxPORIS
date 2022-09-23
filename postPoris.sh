a0e4ced5b77172bacfcb625dcaef890b2e311f0f
Txinto Vaz <txinto.vaz@gtc.iac.es>
2022-09-23 17:47:21 +0000

    Cambiamos el generador para que los csysX_user sean librerías.

--- postPoris.sh
+++ postPoris.sh
@@ -5,6 +5,6 @@ echo "Creando los links para que la clase PORIS se pueda compilar con tu device"
 cd $1/$1.l
 ln -s ../../PORIS/cxxPORIS/PORIS.cpp PORIS.cpp
 ln -s ../../PORIS/cxxPORIS/PORIS.h PORIS.h
-ln -s ../../$1_user/$1_user.l/$1_user.cpp $1_user.cpp
-ln -s ../../$1_user/$1_user.l/$1_user.h $1_user.h
+#ln -s ../../$1_user/$1_user.l/$1_user.cpp $1_user.cpp
+#ln -s ../../$1_user/$1_user.l/$1_user.h $1_user.h
 cd ../..
