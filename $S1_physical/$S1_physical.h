4005d36d017a7389f8eae44313dd7d5ea20eb41e
Txinto Vaz <txinto.vaz@gtc.iac.es>
2022-12-01 18:27:37 +0000

    Adding a message to know how to deactivate PORIS features

--- $S1_physical/$S1_physical.h
+++ $S1_physical/$S1_physical.h
@@ -3,6 +3,18 @@
 
 #include <PORIS.h>
 
+/****** PORIS features deactivation overrides *********/
+/** This is needed for monitoring magnitudes which shall have no modes, and shall 
+ * be written without taking care of its current working mode 
+ 
+Examples:
+#define DEVICENAME_PORIS_DISABLE_setDEVICENAMEMode
+
+Check for these defines at DEVICENAME.l/DEVICENAME.cpp there must be one per each mode or parameter
+in the device (one per monitor)
+
+ */
+
 // In the DEVICENAME.l folder there is a pair of DEVICENAME_stub files (cpp and h)
 // You should copy the stubs here below, and modify them to overwrite them with custom functionaliy added by you
 // As the stubs are automatically generated from the model, you are advised to check
